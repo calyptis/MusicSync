79b40041fe32389c17480dc937b49c3ce58ecf16
chizzali <chizzali@kth.se>
2025-05-15 21:29:07 +0200

    Add additional test example

--- tests/match.py
+++ tests/match.py
@@ -1,11 +1,19 @@
+import json
+
 from music_sync.classes import Song
 from music_sync.spotify.matching import get_best_match
 from music_sync.spotify.utils import get_spotipy_instance
 
 if __name__ == "__main__":
-    song = Song(
-        name="All Eyez On Me (feat. Big Syke)", artist="2Pac", album="All Eyez On Me"
-    )
+    songs = [
+        Song(
+            name="All Eyez On Me (feat. Big Syke)",
+            artist="2Pac",
+            album="All Eyez On Me",
+        ),
+        Song(name="Caruso", artist="Fiorella Mannoia", album="A te (Special Edition)"),
+    ]
     sp = get_spotipy_instance()
-    best_match = get_best_match(sp, song)
-    print(best_match)
+    for song in songs:
+        best_match = get_best_match(sp, song)
+        print(json.dumps(best_match.model_dump(), indent=4))
