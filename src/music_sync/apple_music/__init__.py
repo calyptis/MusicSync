24b65ee330dfbe920ddc936858c432743e670435
chizzali <chizzali@kth.se>
2026-01-15 10:54:40 +0100

    Remove imports

--- src/music_sync/apple_music/__init__.py
+++ src/music_sync/apple_music/__init__.py
@@ -1,3 +1,1 @@
 """Parse Apple Music Library."""
-
-from music_sync.apple_music.cli import main as parse_apple_music_library
--- src/music_sync/spotify/__init__.py
+++ src/music_sync/spotify/__init__.py
@@ -1,3 +1,1 @@
 """Sync Spotify Library."""
-
-from music_sync.spotify.cli import main as sync_spotify_library
